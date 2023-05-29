346a6c8d495d34810afff6ba6eb30c929798d00f
ArkadiuszMigas <arex1470@gmail.com>
2023-05-30 00:14:47 +0200

    homePage

--- app/src/main/java/com/example/imhungry/SignUp.kt
+++ app/src/main/java/com/example/imhungry/SignUp.kt
@@ -8,6 +8,7 @@ import android.widget.Button
 import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class SignUp : AppCompatActivity() {
     private lateinit var firebaseAuth: FirebaseAuth
